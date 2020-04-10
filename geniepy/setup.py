d1e1fa34413a228a9562e34b82b9c202b15b1445
derekkinzo <derek.kinzo@jkegrp.com>
2020-04-10 12:59:54 -0600

    Fix package files for pypi

--- geniepy/setup.py
+++ geniepy/setup.py
@@ -1,11 +1,17 @@
 """Package setup module for setuptools package generation."""
 from setuptools import setup, find_packages
 
-__version__ = "0.0.5"
+__version__ = "0.2"
 setup(
-    name="geniepy",
     version=__version__,
+    name="GeniePy",
     description="Gene-disease trend detection",
+    license="MIT",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    author="The Harvard LAMP Team",
+    maintainer_email="dhk891@g.harvard.edu",
+    url="https://github.com/derekkinzo/genie",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["jsonlines"],
