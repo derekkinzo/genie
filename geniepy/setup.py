ef211b336085bead393635ee24f64670cbd4ee82
derekkinzo <derek.kinzo@jkegrp.com>
2020-04-10 01:17:19 -0600

    Update package files

--- geniepy/setup.py
+++ geniepy/setup.py
@@ -1,21 +1,24 @@
 """Package setup module for setuptools package generation."""
 from setuptools import setup, find_packages
 
-with open('LICENSE') as licenseFile:
+with open("LICENSE") as licenseFile:
     LICENSE = licenseFile.read()
 
-with open('AUTHORS') as authorFile/
+with open("AUTHORS") as authorFile:
     AUTHORS = authorFile.read()
 
 setup(
-    name='geniepy',
-    version='0.01',
-    description='Gene-disease trend detection',
+    name="geniepy",
+    version="0.1",
+    description="Gene-disease trend detection",
     author=AUTHORS,
-    url='',
+    url="",
     license=LICENSE,
-    packages=find_packages(exclude=['tests*', 'docs']),
+    packages=find_packages(exclude=["tests*", "docs"]),
     include_package_data=True,
-    python_requires='>=3.7',
-    keywords='gene disease trend detection'
+    install_requires=[
+        'jsonlines'
+    ]
+    python_requires=">=3.7",
+    keywords="gene disease trend detection",
 )
