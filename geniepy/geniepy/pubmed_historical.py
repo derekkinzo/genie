cc3e20929259fb3e5ef2986b5787213c644b6984
derekkinzo <derek.kinzo@jkegrp.com>
2020-04-09 23:42:51 -0600

    fix linter errors and format files

--- geniepy/geniepy/pubmed_historical.py
+++ geniepy/geniepy/pubmed_historical.py
@@ -28,53 +28,68 @@ def is_xml_article_set(filename: str) -> bool:
     Returns:
         bool -- true if file is compressed pubmed article set
     """
-    if filename.endswith('.xml.gz'):
+    if filename.endswith(".xml.gz"):
         return True
     return False
 
 
 def decompress_article_set(file_path: str, output_file: str):
-    """Unzip articles sets and create xml file."""
-    with gzip.open(file_path, 'rb') as f_in:
-        with open(output_file, 'wb') as f_out:
+    """
+    Unzip articles sets and create xml file.
+
+    Arguments:
+        file_path {str} -- absolute path to article set gz file
+        output_file {str} -- absolute path to created decompressed file
+    """
+    with gzip.open(file_path, "rb") as f_in:
+        with open(output_file, "wb") as f_out:
             shutil.copyfileobj(f_in, f_out)
 
 
 def parse_pubmed_article_set(in_path: str, out_path: str):
-    """Convert xml to json articles."""
+    """
+    Convert xml to json articles.
+
+    Crawls through all files in a directory and create equivalent parsed jsonl file
+    in output_path
+
+    Arguments:
+        in_path {str} -- absolute path to directory containing compressed article sets
+        out_path {str} -- absolute path to desired directory to save output jsonl files
+    """
     filename = os.path.basename(in_path)
     if not is_xml_article_set(filename):
         return
-    xml_file = in_path.replace('.gz', '')
+    xml_file = in_path.replace(".gz", "")
     if not os.path.exists(xml_file):
-        print(f'Extracting {in_path} to {xml_file}')
+        print(f"Extracting {in_path} to {xml_file}")
         decompress_article_set(in_path, xml_file)
 
-    print(f'Parsing {xml_file}')
-    article_list: PubMedArticle = ArticleSetParser.extract_articles(
-        xml_file)
+    print(f"Parsing {xml_file}")
+    article_list: PubMedArticle = ArticleSetParser.extract_articles(xml_file)
 
     # Done with xml - delete to free up space
     os.remove(xml_file)
 
-    output_file = os.path.join(
-        out_path, filename.replace('.xml.gz', '.jsonl'))
-    print(f'Generating {output_file}')
+    output_file = os.path.join(out_path, filename.replace(".xml.gz", ".jsonl"))
+    print(f"Generating {output_file}")
     ArticleSetParser.articles_to_jsonl(article_list, output_file)
 
-    print(f'Compressing file: {output_file}')
-    with open(output_file, 'rb') as jsonl_data:
+    print(f"Compressing file: {output_file}")
+    with open(output_file, "rb") as jsonl_data:
         data_jsonl = jsonl_data.read()
     compressed_data = gzip.compress(data_jsonl)
-    output_file_compressed = output_file + '.gz'
-    with open(output_file_compressed, 'wb') as out_compressed:
+    output_file_compressed = output_file + ".gz"
+    with open(output_file_compressed, "wb") as out_compressed:
         out_compressed.write(compressed_data)
 
     # Done with parsed file - delete to free up space
     os.remove(output_file)
 
-    print(f"PID: {os.getpid()}. File Processed: {output_file}. \
-        Articles Processed {len(article_list)}")
+    print(
+        f"PID: {os.getpid()}. File Processed: {output_file}. \
+        Articles Processed {len(article_list)}"
+    )
     return
 
 
@@ -86,17 +101,17 @@ def concurrent_execution(data_in_dir, data_out_dir, max_workers):
         if is_xml_article_set(filename):
             xml_files.append(os.path.join(data_in_dir, filename))
 
-    print(f'Found {len(xml_files)} PubMed article sets')
+    print(f"Found {len(xml_files)} PubMed article sets")
 
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
-        executor.map(parse_pubmed_article_set,
-                     xml_files,
-                     repeat(data_out_dir))
+        executor.map(parse_pubmed_article_set, xml_files, repeat(data_out_dir))
 
     end_time = datetime.now()
     total_time = end_time - start_time
-    print(f'Concurrent Process {max_workers} \
-        Execution: {len(xml_files)} files, in {total_time}')
+    print(
+        f"Concurrent Process {max_workers} \
+        Execution: {len(xml_files)} files, in {total_time}"
+    )
 
 
 if __name__ == "__main__":
@@ -122,11 +137,14 @@ if __name__ == "__main__":
     try:
         MAX_WORKERS = int(sys.argv[3])
         if MAX_WORKERS < 1 or MAX_WORKERS > 16:
-            print("Invalid number of workers requested - Setting number of \
-                workers to 1")
+            print(
+                "Invalid number of workers requested - Setting number of \
+                workers to 1"
+            )
             MAX_WORKERS = 1
     except TypeError:
         raise Exception(
-            "Max number of processes should be a valid integer. " + ERROR_MSG)
+            "Max number of processes should be a valid integer. " + ERROR_MSG
+        )
 
     concurrent_execution(DATA_IN_DIR, DATA_OUT_DIR, MAX_WORKERS)
