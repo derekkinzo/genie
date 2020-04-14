66ead540b94c284cafff3c39ffd531979eeabb1a
Derek Kinzo <dhk891@g.harvard.edu>
2020-04-14 00:27:35 -0600

    Fix linter unused import error

--- geniepy/geniepy/scripts/geniebootsrap.py
+++ geniepy/geniepy/scripts/geniebootsrap.py
@@ -5,7 +5,6 @@ Scripts in this directory may call genie packages, this module adds genie
 top-level dir to python path for scripts to be able to import genie.
 """
 import sys
-import os
 from pathlib import Path
 
 GENIE_PATH = Path(__file__).resolve().parents[2]
