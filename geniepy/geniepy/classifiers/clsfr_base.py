a95d82bad59da906d71b4889bd38a85b421a3ada
derekkinzo <dhk891@g.harvard.edu>
2020-04-14 22:27:48 -0600

    Initial abstract classifier class

--- geniepy/geniepy/classifiers/clsfr_base.py
+++ geniepy/geniepy/classifiers/clsfr_base.py
@@ -9,7 +9,27 @@ class BaseClsfr(ABC):
     class Attributes(NamedTuple):
         """Classifier Attributes."""
 
-    __slots__ = ["_is_trained"]
+    __slots__ = ["_is_trained", "clsfr_model"]
+
+    @abstractmethod
+    def restore_model(self) -> bool:
+        """
+        Load classifier model from memory.
+
+        Returns:
+            bool -- True if model loaded successfully, False otherwise.
+        """
+        return False
+
+    @abstractmethod
+    def store_model(self) -> bool:
+        """
+        Store classifier model into memory.
+
+        Returns:
+            bool -- True if model saves successfully, False otherwise.
+        """
+        return False
 
     @abstractmethod
     def __init__(self):
@@ -18,15 +38,27 @@ class BaseClsfr(ABC):
 
         Restore classifier model from memory if it exists. Otherwise, train classifier.
         """
-        self._is_trained = False
+        # Attempt to restore classifier model
+        if self.restore_model():
+            self._is_trained = True
 
     @abstractmethod
-    def fit(self, features: [Attributes]):
-        """Train classifier given dataset."""
+    def fit(self, features: [Attributes]) -> str:
+        """
+        Train classifier given dataset.
+
+        Arguments:
+            features {ClassifierAttributes} -- Array of attributes to train classifier.
+
+        Returns:
+            str -- Classifier training results in str, or None if training failed.
+        """
+        return False
 
     @abstractmethod
-    def predict(self, features: Attributes) -> int:
+    def predict(self, features: Attributes):
         """Calculate publication count label."""
+        return None
 
     @property
     def is_trained(self):
--- geniepy/tests/resources/mock.py
+++ geniepy/tests/resources/mock.py
@@ -16,15 +16,39 @@ class MockClsfr(BaseClsfr):
         label: int = 0
         """Example of a integer label."""
 
+    def restore_model(self) -> bool:
+        """
+        Load classifier model from memory.
+
+        Returns:
+            bool -- True if model loaded successfully, False otherwise.
+        """
+        return True
+
+    def store_model(self) -> bool:
+        """
+        Store classifier model into memory.
+
+        Returns:
+            bool -- True if model saves successfully, False otherwise.
+        """
+        return True
+
     def __init__(self):
+        # pylint: disable=W0235
         """
         Initialize classifier.
 
         Restore classifier model from memory if it exists. Otherwise, train classifier.
         """
+        super().__init__()
+        self.mock_prediction = 1
 
-    def fit(self, features: [Attributes]):
+    def fit(self, features: [Attributes]) -> str:
         """Train classifier given dataset."""
+        self.mock_prediction = 2
+        self._is_trained = "String containing training stats"
 
-    def predict(self, features: Attributes) -> int:
+    def predict(self, features: Attributes):
         """Calculate publication count label."""
+        return self.mock_prediction
