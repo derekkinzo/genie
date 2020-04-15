d54d90ddcf6b32ea58819b37644d84626d70ef08
derekkinzo <dhk891@g.harvard.edu>
2020-04-14 20:50:56 -0600

    Fix package entrypoint

--- geniepy/geniepy/__init__.py
+++ geniepy/geniepy/__init__.py
@@ -1,6 +1,5 @@
 """GeniePy Package entry point."""
 from pkg_resources import get_distribution, DistributionNotFound
-from .genie import run_job
 
 try:
     # Change here if project is renamed and does not equal the package name
@@ -17,3 +16,27 @@ __license__ = "MIT"
 
 
 __all__ = ["run_job"]
+
+
+def update_tables():
+    """Scrape internet for new content from sources and update database."""
+    raise NotImplementedError
+
+
+def update_predictions():
+    """Iterate over tables and calculate needed classifier scores."""
+    # trials_classifier = ClassifierClinicalTrials()
+    # run classifier on gene-disease relationship
+    # pcp_data_set = None
+    # pcp_classifier = ClassifierPCP()
+    # publication_prediction = pcp_classifier.predict(pcp_data_set)
+    raise NotImplementedError
+
+
+def run_job():
+    """Cron-job function to scrape sources for updated data and update predictions."""
+    # Make sure tables are up-to-date with available online content
+    update_tables()
+    # Update table with classifier scores
+    update_predictions()
+    raise NotImplementedError
