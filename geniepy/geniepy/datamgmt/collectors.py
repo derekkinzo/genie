425c80ee263c0ec97775a9ecdebd3b389fc4d8b8
derekkinzo <dhk891@g.harvard.edu>
2020-04-17 01:35:29 -0600

    impl collector dao and tests

--- geniepy/geniepy/datamgmt/collectors.py
+++ geniepy/geniepy/datamgmt/collectors.py
@@ -1,24 +1,43 @@
 """Collectors manage a parser and database for a given source."""
+from typing import Generator
 from abc import ABC, abstractmethod
 from pandas import DataFrame
 from geniepy.errors import SchemaError
-from geniepy.datamgmt.daorepositories import BaseDaoRepo, SqlDaoRepo
 from geniepy.datamgmt.parsers import BaseParser, CtdParser
+import geniepy.datamgmt.daorepositories as dr
 
 
 class BaseCollector(ABC):
     """Collectors Abstract Base Class."""
 
-    dao_repo: BaseDaoRepo
-    parser: BaseParser
+    __slots__ = ["_dao_repo", "_parser"]
+
+    @property
+    def tablename(self):
+        """Return the dao repo tablename."""
+        # pylint: disable=no-member
+        return self._dao_repo.tablename
 
     @abstractmethod
     def sync(self):
         """Sync collector database with online sources."""
 
-    def query(self, searchkey=None, limit=10e3) -> DataFrame:
-        """Query DAO for given record."""
-        # return self.dao_repo.query(searchkey, limit)
+    # pylint: disable=bad-continuation
+    def query(
+        self, query: str = None, chunksize: int = dr.CHUNKSIZE
+    ) -> Generator[DataFrame, None, None]:
+        """
+        Query DAO repo and returns a generator of DataFrames with query results.
+
+        Keyword Arguments:
+            query {str} -- Query string. (default: {None} returns all values)
+            chunksize {int} -- Number of rows of dataframe per chunk (default: {10e3})
+
+        Returns:
+            Generator[DataFrame] -- Generator to iterate over DataFrame results.
+        """
+        # pylint: disable=no-member
+        return self._dao_repo.query(query=query, chunksize=chunksize)
 
     def save(self, payload: DataFrame):
         """
@@ -30,17 +49,20 @@ class BaseCollector(ABC):
         Raises:
             SchemaError: dataframe does not conform to table schema.
         """
-        if not self.parser.is_valid(payload):
+        # pylint: disable=no-member
+        if not self._parser.is_valid(payload):
             raise SchemaError
 
 
 class CtdCollector(BaseCollector):
     """Implementation of CTD Collector."""
 
-    def __init__(self):
+    __slots__ = ["_dao_repo", "_parser"]
+
+    def __init__(self, dao_repo: dr.BaseDaoRepo):
         """Initialize collector state."""
-        self.parser = CtdParser()
-        # self.dao_repo = SqlDaoRepo()
+        self._dao_repo = dao_repo
+        self._parser = CtdParser()
 
     def sync(self):
         """Sync collector database with online sources."""
--- geniepy/tests/test_collectors.py
+++ geniepy/tests/test_collectors.py
@@ -3,12 +3,16 @@ import pytest
 from geniepy.datamgmt.collectors import CtdCollector
 from geniepy.errors import SchemaError
 import tests.testdata as td
+from geniepy.errors import DaoError
+import geniepy.datamgmt.daorepositories as dr
 
 
 class TestCtdCollector:
     """PyTest collector test class."""
 
-    collector = CtdCollector()
+    collector = CtdCollector(
+        dr.SqlDaoRepo("sqlite://", dr.CTD_TABLE_NAME, dr.CTD_DAO_SCHEMA)
+    )
 
     def test_constructor(self):
         """Ensure scraper obj constructed successfully."""
@@ -25,14 +29,43 @@ class TestCtdCollector:
         """Test save valid dataframe to collector's DAO doesn't raise error."""
         self.collector.save(payload)
 
-    # @pytest.mark.parametrize("payload", td.CTD_VALID_DF)
-    # def test_query(self, payload):
-    #     """Test queying data."""
-    #     # Save the data first
-    #     self.collector.save(payload)
-    #     # Query data
-    #     digest = payload.Digest[0]
-    #     received_df = self.collector.query(searchkey=digest)
-    #     # Compare data queried to payload
-    #     expected_df = payload.loc[payload.Digest == digest]
-    #     assert expected_df.equals(received_df)
+    @pytest.mark.parametrize("payload", td.CTD_VALID_DF)
+    def test_query(self, payload):
+        """Query valid record."""
+        # Try to create records in db for test if don't exist
+        try:
+            self.collector._dao_repo.save(payload)
+        except DaoError:
+            pass
+        # Attempt to retrieve record
+        digest = payload.Digest[0]
+        query_str = f"SELECT * FROM {self.collector.tablename} WHERE Digest='{digest}';"
+        generator = self.collector.query(query=query_str)
+        for chunk in generator:
+            assert chunk.equals(payload)
+
+    def test_query_non_existent(self):
+        """Query non-existent record should return empty."""
+        # Attempt to retrieve record
+        digest = "INVALID DIGEST"
+        query_str = f"SELECT * FROM {self.collector.tablename} WHERE Digest='{digest}';"
+        generator = self.collector.query(query=query_str)
+        # Make sure generator doesn't return anything since no records in database
+        for _ in generator:
+            assert False  # Should not get here, because generator should return []
+
+    @pytest.mark.parametrize("chunksize", [1, 2, 3, 4])
+    def test_generator_chunk(self, chunksize):
+        """Query all by chunk."""
+        # Try to fill database, in case is empty
+        for record in td.CTD_VALID_DF:
+            try:
+                self.collector.save(record)
+            except DaoError:
+                pass
+        # Get all records in database
+        query_str = f"SELECT * FROM {self.collector.tablename};"
+        generator = self.collector.query(query=query_str, chunksize=chunksize)
+        # Make sure number generator provides df of chunksize each iteration
+        result_df = next(generator)
+        assert result_df.Digest.count()
