9aa0757c9054c9b46d65150c6817417962c74f12
derekkinzo <dhk891@g.harvard.edu>
2020-04-17 00:41:31 -0600

    finished ctd dao repo tests

--- geniepy/tests/test_dao_repositories.py
+++ geniepy/tests/test_dao_repositories.py
@@ -25,22 +25,43 @@ class TestDaoRepo:
         """Attempt to save dataframe with valid schema."""
         self.dao_repo.save(payload)  # Don't expect to return anything
 
-    # Query valid record
     @pytest.mark.parametrize("payload", td.CTD_VALID_DF)
     def test_query(self, payload):
-        """Attempt to save dataframe with valid schema."""
+        """Query valid record."""
         # Try to create records in db for test if don't exist
         try:
-            self.test_save_valid_df(payload)
+            self.dao_repo.save(payload)
         except DaoError:
             pass
         # Attempt to retrieve record
         digest = payload.Digest[0]
         query_str = f"SELECT * FROM {self.dao_repo.tablename} WHERE Digest='{digest}';"
         generator = self.dao_repo.query(query=query_str)
-        for df in generator:
-            assert df.equals(payload)
+        for chunk in generator:
+            assert chunk.equals(payload)
 
-    # Query non-existent record should return empty
+    def test_query_non_existent(self):
+        """Query non-existent record should return empty."""
+        # Attempt to retrieve record
+        digest = "INVALID DIGEST"
+        query_str = f"SELECT * FROM {self.dao_repo.tablename} WHERE Digest='{digest}';"
+        generator = self.dao_repo.query(query=query_str)
+        # Make sure generator doesn't return anything since no records in database
+        for _ in generator:
+            assert False  # Should not get here, because generator should return []
 
-    # Query all by chunk
+    @pytest.mark.parametrize("chunksize", [1, 2, 3, 4])
+    def test_generator_chunk(self, chunksize):
+        """Query all by chunk."""
+        # Try to fill database, in case is empty
+        for record in td.CTD_VALID_DF:
+            try:
+                self.dao_repo.save(record)
+            except DaoError:
+                pass
+        # Get all records in database
+        query_str = f"SELECT * FROM {self.dao_repo.tablename};"
+        generator = self.dao_repo.query(query=query_str, chunksize=chunksize)
+        # Make sure number generator provides df of chunksize each iteration
+        result_df = next(generator)
+        assert result_df.Digest.count()
