e36a8d53809c78651ea1896183d652aba21f7e8b
Gerald Ding <geralddzx@gmail.com>
2020-04-30 12:33:06 -0400

    write bin

--- article_rank/write_binary.py
+++ article_rank/write_binary.py
@@ -4,37 +4,39 @@ import numpy as np
 import csv
 import struct
 
-with open("data/citations2.csv", "r") as rfile:
-    reader = csv.reader(rfile)
-    max = 0
-    for row in reader:
-        if int(row[0]) > max:
-            max = int(row[0])
-        for i in row[1]:
-            if int(i) > max:
-                max = int(i)
+articles = set()
+max = 0
 
-with open("data/citations2.csv", "r") as rfile:
+with open("data/citations.csv", "r") as rfile:
+    reader = csv.reader(rfile)
+    count = 0
+    for row in reader:
+        count += 1
+        if count % 100000 == 0:
+            print(count)
+        cited = int(row[0])
+        articles.add(cited)
+        if cited > max:
+            max = cited
+        for i in row[1:]:
+            citer = int(i)
+            articles.add(citer)
+            if citer > max:
+                max = citer
+
+with open("data/citations.csv", "r") as rfile:
     reader = csv.reader(rfile)
     with open("data/links", "wb") as wfile:
         wfile.write(max.to_bytes(4, signed=False, byteorder="little"))
+        wfile.write(len(articles).to_bytes(4, signed=False, byteorder="little"))
+        print(len(articles))
+        print(max)
+        count = 0
         for row in reader:
             wfile.write(int(row[0]).to_bytes(4, signed=False, byteorder="little"))
             wfile.write(len(row[1:]).to_bytes(4, signed=False, byteorder="little"))
+            count += 1
+            if count % 100000 == 0:
+                print(count)
             for i in row[1:]:
                 wfile.write(int(i).to_bytes(4, signed=False, byteorder="little"))
-
-#                 wfile.write()
-#                 cited.add(row[0])
-#                 for i in row[1:]:
-#                     citer.add(i)
-#                 count += 1
-#                 if count % 100000 == 0:
-#                     print(count)
-#
-#         wfile.write(struct.pack('i', value))
-#
-#
-# for c in cited:
-#     if not c in citer:
-#         print(c)
