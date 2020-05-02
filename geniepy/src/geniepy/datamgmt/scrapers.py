4bf4b8a43f0a095b5673ca13856dd1f506ec5431
derekkinzo <dhk891@g.harvard.edu>
2020-05-02 16:35:10 -0600

    initial impl of pubtator gene

--- geniepy/src/geniepy/datamgmt/scrapers.py
+++ geniepy/src/geniepy/datamgmt/scrapers.py
@@ -1,6 +1,12 @@
 """Scraping module to fetch data from online sources."""
+from pathlib import Path
 from typing import Generator
 from abc import ABC, abstractmethod
+import wget
+import gzip
+import shutil
+import os
+import pandas as pd
 
 
 class BaseScraper(ABC):
@@ -26,6 +32,48 @@ class BaseScraper(ABC):
         Returns:
             Generator -- The generator yielding the data in given chunksizes.
         """
+
+
+class PubtatorGeneScraper(BaseScraper):
+    """Scrape PMID/GENEID from pubtator."""
+
+    TMP_DIR = Path.cwd().joinpath("tmp")
+    FTP_URL = "ftp://ftp.ncbi.nlm.nih.gov/pub/lu/PubTatorCentral/gene2pubtatorcentral.gz"  # noqa
+    PUBTATOR_GZIP_NAME = TMP_DIR.joinpath("pubtator-gene.gzip").resolve()
+    PUBTATOR_CSV_NAME = TMP_DIR.joinpath("pubtator-gene.csv").resolve()
+
+    def __init__(self):
+        self.TMP_DIR.mkdir(parents=True, exist_ok=True)
+
+    def download(self):
+        """Download records from online sources."""
+        wget.download(self.FTP_URL, str(self.PUBTATOR_GZIP_NAME))
+        with gzip.open(self.PUBTATOR_GZIP_NAME, "rb") as f_in:
+            with open(self.PUBTATOR_CSV_NAME, "wb") as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+    def scrape(self, chunksize: int, **kwargs) -> Generator:
+        """
+        Download pmid/geneid data from pubtator.
+
+        ftp://ftp.ncbi.nlm.nih.gov/pub/lu/PubTatorCentral/gene2pubtatorcentral.gz
+        """
+
+        header_names = ["PMID", "Type", "Concept ID", "Mentions", "Resource"]
+        csv_gen = pd.read_csv(
+            self.PUBTATOR_CSV_NAME,
+            chunksize=chunksize,
+            delimiter="\t",
+            names=header_names,
+        )
+        return csv_gen
+
+    def clean_up(self):
+        """Delete all temp files."""
+        if os.path.exists(self.PUBTATOR_GZIP_NAME):
+            os.remove(self.PUBTATOR_GZIP_NAME)
+        if os.path.exists(self.PUBTATOR_CSV_NAME):
+            os.remove(self.PUBTATOR_CSV_NAME)
 
 
 class CtdScraper(BaseScraper):
