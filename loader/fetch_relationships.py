6eaf7953b3b27d381d16835687bb1ec50d117717
Gerald Ding <geralddzx@gmail.com>
2020-05-05 18:41:10 -0400

    load

---
+++ loader/fetch_relationships.py
@@ -0,0 +1,31 @@
+import pdb
+import csv
+import random
+from connection import connection
+from bigquery import client
+
+results = client.query("""
+    SELECT * FROM `harvard-599-trendsetters.Genie.final_table_output_to_ui` LIMIT 1000
+""")
+
+with connection:
+    with connection.cursor() as cur:
+        cur.execute("DELETE FROM relationships;")
+        connection.commit()
+
+        count = 0
+        for row in results:
+            count += 1
+            if count % 5000 == 0:
+                print(count)
+                connection.commit()
+
+            change_recent = None
+            if row[6] == "N":
+                change_recent = False
+            elif row[6] == "Y":
+                change_recent = True
+            cur.execute(
+                "INSERT INTO relationships VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
+                (row[0], row[1][:-1], row[2], row[3], row[4], row[5], change_recent, row[7][:-1], row[8][:-1], row[9], row[10] or 0)
+            )
---
+++ loader/relationships.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE genie;
+
+\c genie;
+CREATE EXTENSION pg_trgm;
+
+DROP TABLE relationships;
+
+CREATE TABLE relationships(
+  id character varying PRIMARY KEY,
+  p2_prob smallint NOT NULL,
+  mesh_id character varying NOT NULL,
+  gene_id integer NOT NULL,
+  disease_name character varying NOT NULL,
+  gene_name character varying NOT NULL,
+  change_recent boolean NOT NULL,
+  recent_prob_change smallint NOT NULL,
+  previous_prob smallint NOT NULL,
+  num_pubs integer NOT NULL,
+  num_citations integer NOT NULL
+);
+
+CREATE INDEX index_relationships_on_id ON relationships USING GIN(id gin_trgm_ops);
+CREATE INDEX index_relationships_on_p2_prob ON relationships USING btree (p2_prob);
+CREATE INDEX index_relationships_on_mesh_id ON relationships USING GIN(mesh_id gin_trgm_ops);
+CREATE INDEX index_relationships_on_disease_name ON relationships USING GIN(disease_name gin_trgm_ops);
+CREATE INDEX index_relationships_on_gene_name ON relationships USING GIN(gene_name gin_trgm_ops);
+CREATE INDEX index_relationships_on_change_recent ON relationships USING btree (change_recent);
+CREATE INDEX index_relationships_on_recent_prob_change ON relationships USING btree (recent_prob_change);
+CREATE INDEX index_relationships_on_previous_prob ON relationships USING btree (previous_prob);
+CREATE INDEX index_relationships_on_num_pubs ON relationships USING btree (num_pubs);
+CREATE INDEX index_relationships_on_num_citations ON relationships USING btree (num_citations);
