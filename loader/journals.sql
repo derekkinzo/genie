ef36dd3fe01ed9bf74b434c4a56167c3f1ce1508
Gerald Ding <geralddzx@gmail.com>
2020-05-05 14:20:16 -0400

    trigram search

--- loader/journals.sql
+++ loader/journals.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE genie;
 
 \c genie;
+CREATE EXTENSION pg_trgm;
 
 -- DROP TABLE journals;
 
@@ -13,3 +14,4 @@ CREATE TABLE journals(
 
 CREATE INDEX index_journals_on_year ON journals USING btree (year);
 CREATE INDEX index_journals_on_count ON journals USING btree (count);
+CREATE INDEX index_journals_on_id_gin ON journals USING GIN(id gin_trgm_ops);
