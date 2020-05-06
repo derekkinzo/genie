7dc6fe2bd4e3de25853107893cea699b86a6bc78
Gerald Ding <geralddzx@gmail.com>
2020-05-06 19:56:43 -0400

    pub

--- loader/fetch_pubs.py
+++ loader/fetch_pubs.py
@@ -4,23 +4,19 @@ import random
 from connection import connection
 from bigquery import client
 
+results = client.query("""
+    SELECT * FROM `harvard-599-trendsetters.aact.publications_cumulative_sum` LIMIT 1000
+""")
+
 with connection:
     with connection.cursor() as cur:
-        for type in ["gene", "disease"]:
-            results = client.query("""
-                SELECT * FROM `harvard-599-trendsetters.aact.cumulative_{}_publications`
-            """.format(type))
+        cur.execute("DELETE FROM pub_sums;")
+        connection.commit()
+        count = 0
 
-            cur.execute("DELETE FROM {}_pubs;".format(type))
-            connection.commit()
-
-            count = 0
-            for row in results:
-                count += 1
-                if count % 5000 == 0:
-                    print(count)
-                    connection.commit()
-                cur.execute(
-                    "INSERT INTO {}_pubs VALUES (%s, %s, %s, %s);".format(type),
-                    (row[0], row[1], row[2] or 0, row[3] or 0)
-                )
+        for row in results:
+            count += 1
+            if count % 10000 == 0:
+                print(count)
+                connection.commit()
+            cur.execute("INSERT INTO pub_sums VALUES (%s, %s, %s, %s);", (row[0], row[1], row[2] or 0, row[3] or 0))
