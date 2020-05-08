64264a5efedfa98917a758820e54c79a0c1df582
Gerald Ding <geralddzx@gmail.com>
2020-05-08 01:21:27 -0400

    r4

--- front-end/genie.py
+++ front-end/genie.py
@@ -58,7 +58,7 @@ def index():
             results = []
             for relationship in relationships:
                 row = list(relationship)
-                row[3] = str(row[3]) + "%"
+                row[4] = str(row[4]) + "%"
                 row[6] = str(row[6]) + "%"
                 row[7] = str(row[7]) + "%"
                 results.append(row)
